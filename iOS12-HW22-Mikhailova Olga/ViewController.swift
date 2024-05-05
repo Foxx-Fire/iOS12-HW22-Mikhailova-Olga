8ecc23c820f1c999430f95b34593c7ec718bbca5
Olga Mikhailova <foxx_fire@mail.ru>
2024-05-06 01:12:17 +0300

    Initial Commit

---
+++ iOS12-HW22-Mikhailova Olga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW22-Mikhailova Olga
+//
+//  Created by FoxxFire on 06.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
